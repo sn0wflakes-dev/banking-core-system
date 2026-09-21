f0f4f1798efe30b70bbcc061cd9434f2096eebab
abdurrahman ali <abdurr.ali17@gmail.com>
2026-09-22 00:54:41 +0700

    fix: formatting interface

--- src/main/java/aji/intern/core/service/KeyRotationService.java
+++ src/main/java/aji/intern/core/service/KeyRotationService.java
@@ -4,8 +4,6 @@ import aji.intern.core.rest.dto.security.*;
 
 public interface KeyRotationService {
     RegisterServiceResponse registerService(RegisterServiceRequest request);
-
     RetrieveKeyResponse retrieveKey(RetrieveKeyRequest request);
-
     RotateKeyResponse rotateKey(RotateKeyRequest request);
 }
